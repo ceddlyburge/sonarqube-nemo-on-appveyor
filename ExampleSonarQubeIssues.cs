592174c4b83f44e593c971e31db3319fdb80e136
CeddlyBurge <CeddlyBurge@gmail.com>
2016-12-19 08:25:23 +0000

    Create solution with some example sonarqube issues

---
+++ ExampleSonarQubeIssues.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sonarqube_nemo_on_appveyor
+{
+    public class ExampleSonarQubeIssues
+    {
+        private void EmptyMethod()
+        {
+
+        }
+
+        public int ExceptionInProperty
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Example()
+        {
+            String unusedString;
+        }
+    }
+}
