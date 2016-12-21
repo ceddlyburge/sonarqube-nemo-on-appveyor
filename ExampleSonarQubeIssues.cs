a66f1199f648bdf15b11aad215c0c439e9e3a563
CeddlyBurge <CeddlyBurge@gmail.com>
2016-12-21 18:22:28 +0000

    Add link to sonar qube issues page

--- ExampleSonarQubeIssues.cs
+++ ExampleSonarQubeIssues.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace sonarqube_nemo_on_appveyor
 {
+    // The full list of C# SonarQube rules is at http://dist.sonarsource.com/reports/coverage/rules/csharpsquid_rules_coverage.html
     public class ExampleSonarQubeIssues
     {
         private void EmptyMethod()
